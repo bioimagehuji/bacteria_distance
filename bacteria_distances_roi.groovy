ace8c4a86815d177c56cf9880c166183f31040ed
bioimagehuji <151451831+bioimagehuji@users.noreply.github.com>
2023-12-07 09:33:04 +0200

    full names to variables

--- bacteria_distances_roi.groovy
+++ bacteria_distances_roi.groovy
@@ -6,37 +6,37 @@
 
 
 def user_annotation = getSelectedObject()
-def nuc_detections = PathObjects.createAnnotationObject(user_annotation.getROI())
-def nuc_annotations = PathObjects.createAnnotationObject(user_annotation.getROI())
-nuc_detections.setLocked(true)
-nuc_annotations.setLocked(true)
-//nuc_detections.setName("nuceli_detections")
-//nuc_annotations.setName("nuceli_annotations")
-user_annotation.addChildObject(nuc_annotations)
-user_annotation.addChildObject(nuc_detections)
+def nuclei_detections = PathObjects.createAnnotationObject(user_annotation.getROI())
+def nuclei_annotations = PathObjects.createAnnotationObject(user_annotation.getROI())
+nuclei_detections.setLocked(true)
+nuclei_annotations.setLocked(true)
+//nuclei_detections.setName("nuceli_detections")
+//nuclei_annotations.setName("nuceli_annotations")
+user_annotation.addChildObject(nuclei_annotations)
+user_annotation.addChildObject(nuclei_detections)
 
 // Detect cell nuclei from DAPI
-selectObjects(nuc_detections)
+selectObjects(nuclei_detections)
 runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage":"LED-DAPI_Q","requestedPixelSizeMicrons":0.5,"backgroundRadiusMicrons":8.0,"backgroundByReconstruction":true,"medianRadiusMicrons":0.0,"sigmaMicrons":1.5,"minAreaMicrons":10.0,"maxAreaMicrons":400.0,"threshold":10.0,"watershedPostProcess":true,"cellExpansionMicrons":0.1,"includeNuclei":true,"smoothBoundaries":true,"makeMeasurements":false}')
-for (PathObject childObject : nuc_detections.getChildObjects()) {
+for (PathObject childObject : nuclei_detections.getChildObjects()) {
     childObject.setPathClass(getPathClass("Nucleus"))
-    nucleus_ann = PathObjects.createAnnotationObject(childObject.getROI(), childObject.getPathClass())
-    nucleus_ann.setLocked(true)
-    nuc_annotations.addChildObject(nucleus_ann)
+    nucleus_annotation = PathObjects.createAnnotationObject(childObject.getROI(), childObject.getPathClass())
+    nucleus_annotation.setLocked(true)
+    nuclei_annotations.addChildObject(nucleus_annotation)
 }
-user_annotation.removeChildObject(nuc_detections)
+user_annotation.removeChildObject(nuclei_detections)
 
 
 // Duplicate nuclei rectangle annotation (without nuclei)
-def bac_annotation = PathObjects.createAnnotationObject(user_annotation.getROI())
-//bac_annotation.setName('bacteria_annotation');
-user_annotation.addChildObject(bac_annotation)
+def bacteria_annotation = PathObjects.createAnnotationObject(user_annotation.getROI())
+//bacteria_annotation.setName('bacteria_annotation');
+user_annotation.addChildObject(bacteria_annotation)
 
 
 // Detect bacteria from TRITC
-selectObjects(bac_annotation)
+selectObjects(bacteria_annotation)
 runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage":"LED-Tritc_Q (C3)","requestedPixelSizeMicrons":0.1,"backgroundRadiusMicrons":32.0,"backgroundByReconstruction":true,"medianRadiusMicrons":0.0,"sigmaMicrons":0.0,"minAreaMicrons":0.5,"maxAreaMicrons":10.0,"threshold":90.0,"watershedPostProcess":true,"cellExpansionMicrons":0.1,"includeNuclei":true,"smoothBoundaries":true,"makeMeasurements":false}')
-for (PathObject childObject : bac_annotation.getChildObjects()) {
+for (PathObject childObject : bacteria_annotation.getChildObjects()) {
     childObject.setPathClass(getPathClass("Bacterium"))
 }
 
